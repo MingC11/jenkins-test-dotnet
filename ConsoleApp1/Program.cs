28458525739611d07d76c13c13af4716eef73604
Ming Chin <ming.chin@fisglobal.com>
2022-03-29 13:03:17 -0600

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("This is running in J E N K I N S");
+        }
+    }
+}
